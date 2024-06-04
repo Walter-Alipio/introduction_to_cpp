d3614cd58818cd02709b150fb5a134ae627738fb
Walter <silvawalteralipio@gmail.com>
2024-06-03 21:50:45 -0300

    initial

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main (){
+   cout << "*************************************" << endl;
+   cout << "* Bem-vindo ao jogo da adivinhação! *" << endl;
+   cout << "*************************************" << endl;
+}
