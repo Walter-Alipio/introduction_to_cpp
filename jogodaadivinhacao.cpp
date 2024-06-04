741912ca2b8163cd7ea7fa4843849b3a6d5e14d2
Walter <silvawalteralipio@gmail.com>
2024-06-03 22:39:54 -0300

    second class: conditional and loops

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
 using namespace std;
 
-
-
-int main (){
+int main()
+{
    cout << "*************************************" << endl;
    cout << "* Bem-vindo ao jogo da adivinhação! *" << endl;
    cout << "*************************************" << endl;
+
+   const int NUMERO_SECRETO = 42;
+
+   bool nao_acertou = true;
+   int tentativas = 0;
+
+   while (nao_acertou)
+   {
+      tentativas++;
+      int chute;
+      cout<<"Tentativa " << tentativas << endl;
+      cout << "Qual é o seu chute?" << endl;
+      cin >> chute;
+      cout << "O valor do seu chute é :" << chute << endl;
+
+      bool acertou = chute == NUMERO_SECRETO;
+      bool maior = chute > NUMERO_SECRETO;
+
+      if (acertou)
+      {
+         cout << "Parabéns! Você acertou o número secreto!" << endl;
+         nao_acertou = false;
+      }
+      else if (maior)
+      {
+         cout << "Seu chute foi maior do que o número secreto!" << endl;
+      }
+      else
+      {
+         cout << "Seu chute foi menor que o número secreto!" << endl;
+      }
+   }
+
+   cout<<"Fim de jogo!"<<endl;
+   cout<<"Você acertou o número secreto em " << tentativas <<" tentativas" << endl;
 }
