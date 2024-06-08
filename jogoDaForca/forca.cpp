e8b207ecae63e203546737f190983c8c8547898b
Walter <silvawalteralipio@gmail.com>
2024-06-07 22:58:23 -0300

    Add MakerFile to expecify compilation Add metodo letra_existe Add dictionary to shouts maded

--- jogoDaForca/forca.cpp
+++ jogoDaForca/forca.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+map<char,bool> chutou;
+
+bool letra_existe(char chute){
+    for(char letra : PALAVRA_SECRETA ){
+        if (chute == letra){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
 int main(){
-    string palavra_secreta = "MELANCIA";
-    cout << palavra_secreta << endl;
+
+    bool nao_acertou = true;
+    bool nao_inforcou = true;
+
+    while(nao_acertou && nao_inforcou){
+        for (char letra : PALAVRA_SECRETA)
+        {
+            if(chutou[letra]){
+                cout << letra << " ";
+            }
+            else{
+                cout << "_ ";
+            }
+        }
+        
+
+        char chute;
+        cin >> chute;
+
+        chutou[chute] = true;
+
+        if (letra_existe(chute)){
+            cout << "Você acertou! Seu chute está na palavra." << endl;
+        }
+        else{
+            cout << "Você errou! Seu chute está na palavra." << endl;
+        }
+    }
 }    
 
